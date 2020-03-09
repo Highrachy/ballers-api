79c5cbbd7861f14dd580d589d1c511fe45ce5644
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-03-09 07:55:00 +0100

    chore: fixed comments [Finishes #171565994]

--- routes/welcome.js
+++ routes/welcome.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-// get phone number belonging to a customer's email
 router.get('/', (req, res) => {
   res.status(200).send({ success: true, message: 'Welcome to Ballers API endpoint' });
 });
