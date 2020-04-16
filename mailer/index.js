06b37b1c69d5c7ad5abedbf86cce9c0380c15e7b
Haruna Popoola <haruna.popoola@yahoo.com>
2020-04-16 12:36:21 +0100

    create mailer template

---
+++ mailer/index.js
@@ -0,0 +1,13 @@
+const EMAIL_CONTENT = {
+  DEFAULT: {
+    buttonText: 'Button Text',
+    contentBottom: 'Bottom content is here',
+    contentTop: 'This is the deafault top content',
+    firstName: '[First Name]',
+    greeting: 'Welcome',
+    link: 'sample link',
+    subject: 'Email Subject is Here',
+  },
+};
+
+export default EMAIL_CONTENT;
