ff8a4ebb5f10f66c7b0dc1dc864f387755d516d7
Haruna Popoola <haruna.popoola@yahoo.com>
2020-06-17 09:01:50 +0100

    create blissville mail

--- mailer/mailer.text.template.js
+++ mailer/mailer.text.template.js
@@ -8,16 +8,28 @@ export default ({ greeting, firstName, contentTop, contentBottom, buttonText, li
       : '';
   content += (contentTop && contentTop.replace('<br>', '\n')) || '';
   content += contentBottom ? `\n\n${contentBottom.replace('<br>', '\n')}` : '';
-
+  console.log('content', content, greetings, button);
   // Note: The text is formatted as it should appear on the device
   return `
 
-${greetings},
+  Good day,
 
-${content.replace(/<[^>]+>/g, '')}
+  Hope this mail meets you well.
 
-${button}
+  This is to introduce to you our Investment and Technology Company.
 
-Thank you,
-BALLERS TEAM.`;
+  Highrachy Investment & Technology Ltd. is a technology and real estate company and our vision is to be a globally known one stop shop for value within the real estate and technology industries.
+
+  We offer you quality investment opportunities through our top-notch structures built with style, taste, and functionality in mind.
+
+  We have currently available 4units of 3bedroom flats with a maids quarters located off Orchid hotel road Chevron Lekki, Lagos.
+
+  We are available for site inspection whenever you are ready, below are pictures of our ongoing project.
+
+  To book a visit, please send me an email or call +234 7085389504.
+
+  Looking forward to hearing from you.
+
+  Thank you.
+  Best Regards`;
 };
