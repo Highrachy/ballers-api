64d36d15766250e6600bd6830b06f0c91862d937
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-09-04 04:01:09 +0100

    fix non blocking

--- server/schemas/visitation.schema.js
+++ server/schemas/visitation.schema.js
@@ -9,7 +9,15 @@ const propertyVisitationSchema = Joi.object({
   visitorName: Joi.string().label('Name').required(),
   visitorEmail: Joi.string().label('Email address').email().optional(),
   visitorPhone: Joi.string().label('Phone').min(11).max(14).required(),
-  visitDate: Joi.date().greater(today).label('Visit Date').required(),
+  visitDate: Joi.date()
+    .greater(today)
+    .label('Visit Date')
+    .required()
+    .messages({
+      'date.greater': `"Visit Date" should a date later than ${new Date(
+        Date.parse(today),
+      ).toUTCString()}`,
+    }),
 });
 
 export default propertyVisitationSchema;
