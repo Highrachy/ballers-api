ab6c9cfa2776b5f7264f02733e39f1fb1073169b
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-10-11 23:34:19 +0100

    test fixes

--- test/controllers/offer.controller.test.js
+++ test/controllers/offer.controller.test.js
@@ -17,6 +17,7 @@ import {
   getTodaysDateInWords,
 } from '../../server/helpers/dates';
 import * as MailService from '../../server/services/mailer.service';
+import EMAIL_CONTENT from '../../mailer';
 
 useDatabase();
 
@@ -404,6 +405,8 @@ describe('Offer Controller', () => {
             expect(res.body.offer.enquiryInfo._id).to.be.eql(enquiryId.toString());
             expect(res.body.offer.propertyInfo._id).to.be.eql(propertyId1.toString());
             expect(sendMailSpy.callCount).to.eq(2);
+            expect(sendMailSpy).to.have.be.calledWith(EMAIL_CONTENT.OFFER_RESPONSE_VENDOR);
+            expect(sendMailSpy).to.have.be.calledWith(EMAIL_CONTENT.OFFER_RESPONSE_USER);
             done();
           });
       });
@@ -419,6 +422,7 @@ describe('Offer Controller', () => {
             expect(res).to.have.status(412);
             expect(res.body.success).to.be.eql(false);
             expect(res.body.message).to.be.eql('You cannot accept offer of another user');
+            expect(sendMailSpy.callCount).to.eq(0);
             done();
           });
       });
@@ -433,6 +437,7 @@ describe('Offer Controller', () => {
             expect(res).to.have.status(403);
             expect(res.body.success).to.be.eql(false);
             expect(res.body.message).to.be.eql('Token needed to access resources');
+            expect(sendMailSpy.callCount).to.eq(0);
             done();
           });
       });
@@ -448,6 +453,7 @@ describe('Offer Controller', () => {
           .end((err, res) => {
             expect(res).to.have.status(400);
             expect(res.body.success).to.be.eql(false);
+            expect(sendMailSpy.callCount).to.eq(0);
             done();
             Offer.findByIdAndUpdate.restore();
           });
@@ -467,6 +473,7 @@ describe('Offer Controller', () => {
               expect(res.body.success).to.be.eql(false);
               expect(res.body.message).to.be.eql('Validation Error');
               expect(res.body.error).to.be.eql('"Offer Id" is not allowed to be empty');
+              expect(sendMailSpy.callCount).to.eq(0);
               done();
             });
         });
@@ -483,6 +490,7 @@ describe('Offer Controller', () => {
               expect(res.body.success).to.be.eql(false);
               expect(res.body.message).to.be.eql('Validation Error');
               expect(res.body.error).to.be.eql('"Signature" is not allowed to be empty');
+              expect(sendMailSpy.callCount).to.eq(0);
               done();
             });
         });
