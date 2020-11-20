204b733a9d035ca6ea267957bfedc55e1fd7b562
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-11-20 01:27:10 +0100

    add vendor object to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -63,6 +63,14 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    vendor: {
+      companyName: {
+        type: String,
+      },
+      companyAddress: {
+        type: String,
+      },
+    },
     email: {
       type: String,
       unique: true,
