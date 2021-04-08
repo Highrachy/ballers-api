96164b60db4dc0a2f4b444929ff62db34490b0d1
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2021-04-08 11:34:55 +0100

    add new property gallery image to top of array

--- server/services/property.service.js
+++ server/services/property.service.js
@@ -594,11 +594,12 @@ export const addGallery = async (imageDetails) => {
   if (gallery.length > 0) {
     throw new ErrorHandler(httpStatus.PRECONDITION_FAILED, 'Image with title already exists');
   }
+  const newGallery = [{ title: imageDetails.title, url: imageDetails.url }, ...property.gallery];
 
   try {
     return Property.findByIdAndUpdate(
       property._id,
-      { $push: { gallery: { title: imageDetails.title, url: imageDetails.url } } },
+      { $set: { gallery: newGallery } },
       { new: true },
     );
   } catch (error) {
