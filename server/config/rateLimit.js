2f0858bbbf8a86fc34ab9afc4b28dcf21aab9464
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2021-04-18 23:02:44 +0100

    disable rate limit for test environment

--- server/config/rateLimit.js
+++ server/config/rateLimit.js
@@ -1,12 +1,12 @@
 import rateLimit from 'express-rate-limit';
 
-const timeFrameInHours = 24;
-const maxNoOfRequests = 5000;
+const timeFrameInHours = 1;
+const maxNoOfRequests = 500;
 
 const rateLimiter = rateLimit({
   windowMs: timeFrameInHours * 60 * 60 * 1000,
   max: maxNoOfRequests,
-  message: `You have exceeded the ${maxNoOfRequests} requests in ${timeFrameInHours}hrs limit!`,
+  message: `You have exceeded the ${maxNoOfRequests} requests in ${timeFrameInHours} hour limit!`,
   headers: true,
 });
 
--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('combined', { stream: winston.stream.write }));
-app.use(rateLimiter);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use('/api/', rateLimiter);
+}
 
 app.set('view engine', 'ejs');
 
